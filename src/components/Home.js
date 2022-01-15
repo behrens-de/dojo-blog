6c983daaa1f92d8d7c33cd1f12a611b3031c7fb5
Jan Behrens <dev@jpbehrens.de>
2022-01-15 22:10:49 +0100

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,17 @@ import { useState } from "react"
 const Home = () => {
 
     // useState Hook
-    let [name, setName] = useState('Mario')
+    const [name, setName] = useState({ firstname: 'Jan', lastname: 'Behrens' })
+    const [blogs, setBlogs] = useState([
+        { id: 1, title: 'my new website', body: 'lorem ipsum', author: 'Jan Behrens' },
+        { id: 2, title: 'wilcome party!', body: 'lorem ipsum', author: 'Jan Behrens' },
+        { id: 3, title: 'Web dev tips', body: 'lorem ipsum', author: 'Jan Behrens' },
+    ])
+
 
     const handleClickEvent = (event) => {
         console.log('EVENT: ', event)
-        setName('Mario 2')
+        setName({ ...name, lastname: 'Buretti' })
     }
 
     const handleClickParam = (param) => {
@@ -14,10 +20,18 @@ const Home = () => {
     }
 
     return (<div className="Home">
-        <div className="container">
-
+        <div  className="container">
             <h1>HALLO ich bin HOME</h1>
-            <p>{name}</p>
+            
+            {blogs.map(blog => {
+               return  <div className="blog-preview" key={blog.id}>™
+                   <h2>{blog.title}</h2>
+                   <p>{blog.body}</p>
+                   <span>von: {blog.author}</span>
+                   </div>
+            })}
+
+            <p>{name.firstname} {name.lastname}</p>
             <button onClick={handleClickEvent}>Event: Click me!</button>
             <button onClick={() => handleClickParam('Argument')}>Param: Click me!</button>
         </div>
