e87286e9d151feb7364b400918c763082dde93b6
Jan Behrens <dev@jpbehrens.de>
2022-01-16 22:42:56 +0100

    update: cleanup useFetch with AbortController

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -8,20 +8,33 @@ const useFetch = (url) => {
     const [error, setError] = useState(null);
     // UseEffect
     useEffect(() => {
-        fetch(url) // <= API ENDPOINT
-            .then(response => {
-                if (!response.ok) throw Error('Response is not ok');
-                return response.json();
-            })
-            .then(data => {
-                setData(data);
-                setIsLoading(false);
-                setError(null);
-            })
-            .catch(error => {
-                setError(error.message);
-                setIsLoading(false);
-            })
+
+        const abortCtlr = new AbortController();
+
+        setTimeout(() => { // <= for testing the loading 
+            fetch(url, { signal: abortCtlr.signal }) // <= API ENDPOINT
+                .then(response => {
+                    if (!response.ok) throw Error('Response is not ok');
+                    return response.json();
+                })
+                .then(data => {
+                    setData(data);
+                    setIsLoading(false);
+                    setError(null);
+                })
+                .catch(error => {
+                    if (error.name === 'AbortError') {
+                        console.log('fetch abort');
+                    }
+                    else {
+                        setError(error.message);
+                        setIsLoading(false);
+                    }
+                })
+            // CLEAN UP
+        }, 2000);
+        
+        return () => abortCtlr.abort();
 
     }, [url]); // <= beim ersten laden 
 
