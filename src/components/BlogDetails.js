cd879b5e6ee490cc76c7b9ccb71c124ca6326fff
Jan Behrens <dev@jpbehrens.de>
2022-01-18 21:39:15 +0100

    create Delete Function

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,9 +1,21 @@
-import { useParams } from 'react-router-dom'
+import { useHistory, useParams } from 'react-router-dom'
 import useFetch from '../hooks/useFetch';
 const BlogDetails = () => {
 
     const { id } = useParams();
     const { data: blog, isLoading, error } = useFetch('http://localhost:8000/blogs/' + id);
+
+    const history = useHistory();
+
+    const handleClick = () => {
+        // Delete the Post
+        const endpoint = `http://localhost:8000/blogs/${id}`
+        fetch(endpoint,{
+            method: 'DELETE'
+        }).then(()=>{
+            history.push('/');
+        })
+    }
 
     const jsx = (<div className="container">
         {isLoading && <div>Loading</div>}
@@ -13,6 +25,7 @@ const BlogDetails = () => {
                 <h2>{blog.title}</h2>
                 <span className='author'>von: {blog.author}</span>
                 <div>{blog.body}</div>
+                <button className='delete-button' onClick={handleClick}>Delete</button>
             </article>
         )}
     </div>);
